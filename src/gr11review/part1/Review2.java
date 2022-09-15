d7048755988a51d736595a0d0485d27376d52370
noahlin34 <noahlin23@ycdsbk12.ca>
2022-09-15 19:18:12 -0400

    review2

--- src/gr11review/part1/Review2.java
+++ src/gr11review/part1/Review2.java
@@ -1,1 +1,47 @@
 package gr11review.part1;
+import java.io.*;
+
+public class Review2{
+
+
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("0 - print a joke about your hair");
+        System.out.println("1 - print a joke about your feet");
+        System.out.println("2 - print a joke about your clothes");
+        System.out.println("3 - print a joke about your teacher");
+
+        System.out.print("Chose a menu option: ");
+
+        int  userselection = Integer.parseInt(keyboard.readLine());
+
+        if(userselection == 0) {
+            System.out.println("Why do bees have sticky hair?");
+            System.out.println("They always use honeycombs.");
+
+        } else {
+            if(userselection == 1) {
+            System.out.println("Why do bees have sticky hair?");
+            System.out.println("They always use honeycombs.");
+            } else {
+                if(userselection == 2) {
+                    System.out.println("Why do bees have sticky hair?");
+                    System.out.println("They always use honeycombs.");
+                } else {
+                    if(userselection == 3) {
+                        System.out.println("Why do bees have sticky hair?");
+                        System.out.println("They always use honeycombs.");
+                    } else {
+                        System.out.print("Invalid menu option");
+                    }
+                }
+            }
+        }
+
+
+
+    }
+}
